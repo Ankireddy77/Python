3339427c063247bbef7fec45917cec7de9b76584
Ankireddy77 <sirigireddy.a@intimetec.com>
2025-07-03 14:26:58 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,51 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+2 +3
+5
+print ('Anki')
+Anki
+print ('c:\docs\anki')
+c:\docsnki
+print (r'C:\docs\anki')
+C:\docs\anki
+8 // 2
+4
+>>> 8/2
+4.0
+>>> 2**3
+8
+>>> X = 2
+>>> y =3
+>>> X + y
+5
+>>> name = Anki
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    name = Anki
+NameError: name 'Anki' is not defined
+>>> name = 'Youtube'
+>>> name [1]
+'o'
+>>> nums =[35,25,23,45,65]
+>>> nums
+[35, 25, 23, 45, 65]
+>>> names =['anki','vemu','anil']
+>>> names
+['anki', 'vemu', 'anil']
+>>> mil = [ nums, names]
+>>> mil
+[[35, 25, 23, 45, 65], ['anki', 'vemu', 'anil']]
+>>> nums.append (45)
+>>> nums
+[35, 25, 23, 45, 65, 45]
+>>> nums.insert(2,44)
+>>> nums
+[35, 25, 44, 23, 45, 65, 45]
+>>> nums.pop(1)
+25
+>>> nums
+[35, 44, 23, 45, 65, 45]
+>>> min(nums)
+23
+>>> max (nums)
+65
