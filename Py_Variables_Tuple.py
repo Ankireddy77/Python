84dde1928bbebdf6af8cce83b95bb950246df613
Ankireddy77 <sirigireddy.a@intimetec.com>
2025-07-03 23:17:13 +0530

    Add files via upload

---
+++ Py_Variables_Tuple.py
@@ -0,0 +1,75 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> set ={25,35,24,65}
+>>> set
+{24, 25, 35, 65}
+>>> tup = (35,33,25,36,38)
+>>> t
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    t
+NameError: name 't' is not defined
+>>> tup
+(35, 33, 25, 36, 38)
+>>> tup[1]
+33
+>>> help
+Type help() for interactive help, or help(object) for help about object.
+>>> help()
+Welcome to Python 3.13's help utility! If this is your first time using
+Python, you should definitely check out the tutorial at
+https://docs.python.org/3.13/tutorial/.
+
+Enter the name of any module, keyword, or topic to get help on writing
+Python programs and using Python modules.  To get a list of available
+modules, keywords, symbols, or topics, enter "modules", "keywords",
+"symbols", or "topics".
+
+Each module also comes with a one-line summary of what it does; to list
+the modules whose name or summary contain a given string such as "spam",
+enter "modules spam".
+
+To quit this help utility and return to the interpreter,
+enter "q", "quit" or "exit".
+
+help> quit
+
+You are now leaving help and returning to the Python interpreter.
+If you want to ask for help on a particular object directly from the
+interpreter, you can type "help(object)".  Executing "help('string')"
+has the same effect as typing a particular string at the help> prompt.
+>>> x= 5
+y= 6
+x+y
+11
+num =56
+id(num)
+140705668156040
+a= 10
+b= 1
+id (a)
+140705668154568
+id (10)
+140705668154568
+id (b)
+140705668154280
+b = a
+id (b)
+140705668154568
+k = 10
+id (k)
+140705668154568
+a = 9
+id(a)
+140705668154536
+id (b)
+140705668154568
+b = 8
+id (b)
+140705668154504
+PI =3.14
+PI
+3.14
+type (PI)
+<class 'float'>
+ \
